5fbc39c0064035350a2a18078d6fd11040d78597
Siddharthnilekar <nilekarsiddharth26@gmail.com>
2024-03-16 22:46:15 +0530

    assign subscriber to trainer

--- main/admin.py
+++ main/admin.py
@@ -66,3 +66,7 @@ admin.site.register(models.Notify,NotifyAdmin)
 class NotifUserStatusAdmin(admin.ModelAdmin):
 	list_display=('notif','user','status')
 admin.site.register(models.NotifUserStatus,NotifUserStatusAdmin)
+
+class AssignSubscriberAdmin(admin.ModelAdmin):
+	list_display=('subscriber','trainer')
+admin.site.register(models.AssignSubscriber,AssignSubscriberAdmin)
